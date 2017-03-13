77c41f51646c1b9cf9f77670ddc31647925ad2dc
Amrait <zelrik@meta.ua>
2017-03-13 19:42:00 +0200

    Домашнє завдання 7-го тижня

---
+++ src/main/java/kozak/Test.java
@@ -0,0 +1,7 @@
+package kozak;
+
+/**
+ * Created by Олексій on 08.03.2017.
+ */
+public class Test {
+}
---
+++ src/main/java/kozak/servlets/HelloServlet.java
@@ -0,0 +1,7 @@
+package kozak.servlets;
+
+/**
+ * Created by Олексій on 08.03.2017.
+ */
+public class HelloServlet {
+}
